46e38fb8249cb01c5f1aab98113448fd75b809d3
João Costa <148901636+joaopedrocosta2004@users.noreply.github.com>
2024-10-09 19:17:46 +0100

    (Joao Costa) - App: added the Removal button and correct the path to the database

--- App/registration.py
+++ App/registration.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import sqlite3
 import pandas as pd
+import subprocess
 
 window = tk.Tk()
 window.title("User Registration")
-window.geometry("500x300")
+window.geometry("500x400")
 
 def user_registration():
-    con = sqlite3.connect('users.db')
+    con = sqlite3.connect('../users.db')
     cur = con.cursor()
 
     # Insert user data into the database
@@ -29,7 +30,8 @@ def user_registration():
     phone_entry.delete(0, "end")
 
 def user_export():
-    con = sqlite3.connect('users.db')
+    con = sqlite3.connect('../users.db')
+
     cur = con.cursor()
 
     # Fetch all user data from the database
@@ -38,10 +40,13 @@ def user_export():
 
     # Create a DataFrame from the fetched data
     users_registered_df = pd.DataFrame(users_registered, columns=['First Name', 'Second Name', 'Email', 'Phone', 'id'])
-    users_registered_df.to_excel('users_registered.xlsx', index=False)
+    users_registered_df.to_excel('../users_registered.xlsx', index=False)
 
     # Close the connection
     con.close()
+
+def user_removal():
+    subprocess.run(['python', 'Deletion.py'])   
 
 # Create labels and entry boxes for user input
 label_first_name = tk.Label(window, text="First Name:")
@@ -74,5 +79,8 @@ registration_button.grid(row=4, column=0, padx=10, pady=10, ipadx=80)
 
 export_button = tk.Button(text="Export to Excel", command=user_export)
 export_button.grid(row=5, column=0, padx=10, pady=10, ipadx=80)
+
+removal_button = tk.Button(text="Remove User", command=user_removal)
+removal_button.grid(row=6, column=0, padx=10, pady=10, ipadx=80)
 
 window.mainloop()
